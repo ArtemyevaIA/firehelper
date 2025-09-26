4b02181c2dea73f4a2fba88acdbfc1a636ec5371
ArtemyevaIA <nickart.artemyev@gmail.com>
2025-09-26 14:14:29 +0700

    4

--- firehelper.lua
+++ firehelper.lua
@@ -175,7 +175,7 @@ function main()
     local cnt_client = check_client:fetch({}, "a")
     if cnt_client['cnt'] == '0' then
         lastlogin = os.date('%d.%m.%Y')..' '..os.date('%H:%M:%S', os.time() - (UTC * 3600))
-        assert(conn:execute("INSERT INTO clients (nick, tlg_id, firehelper, lastlogin) VALUES ('"..who_nick.."', '0', '1', '"..lastlogin.."')"))
+        assert(conn:execute("INSERT INTO clients (nick, tlg_id, firehelper, lastlogin, ver) VALUES ('"..who_nick.."', '0', '1', '"..lastlogin.."', '"..thisScript().version.."')"))
         assert(conn:execute("INSERT INTO firehelp (nick, give, stats) VALUES ('"..who_nick.."', '0','0')"))
     else
         local client = assert(conn:execute("select c.nick, c.tlg_id, f.give, f.stats from clients c join firehelp f on c.nick = f.nick WHERE c.nick = '"..who_nick.."'"))
@@ -186,7 +186,7 @@ function main()
         if tlg_id ~= '0' then tlg_send = true end
 
         lastlogin = os.date('%d.%m.%Y')..' '..os.date('%H:%M:%S', os.time() - (UTC * 3600))
-        assert(conn:execute("UPDATE clients SET lastlogin = '"..lastlogin.."' WHERE nick = '"..who_nick.."'"))
+        assert(conn:execute("UPDATE clients SET lastlogin = '"..lastlogin.."', ver = '"..thisScript().version.."' WHERE nick = '"..who_nick.."'"))
     end
 
     sampAddChatMessage('', 0x7FFFD4)
